95e290c975cf011292d7cce4c5213659e8a5bff7
Araceliz Gomes <gomesaraceliz@gmail.com>
2019-01-07 12:30:43 -0500

    Update isUnique.py

--- isUnique.py
+++ isUnique.py
@@ -10,3 +10,21 @@ def isUnique(s):
 
 str="abcbdefg"
 print(isUnique(str))
+
+
+#done again 1/7/19
+#return True is string of Unique Characters
+def Unique(s):
+  if s == '':
+    output = "list is null"
+  arr = list(s)
+  output = "Is Unique"
+  for i in range(len(arr)):
+    for j in range(i+1,len(arr)-1):
+      if (arr[i] == arr[j]):
+        output = "Is not Unique"
+  
+  return output
+
+
+print(Unique("abs"))        
